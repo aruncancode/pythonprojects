919b8846ed1e803c2e92d35a45a8061b291766a5
Arun Arjunan <arun.arjunan1@outlook.com>
2020-01-23 19:04:23 +0800

    gets raw data

--- TwtichSubCount/webscrape.py
+++ TwtichSubCount/webscrape.py
@@ -1,6 +1,16 @@
 #twitch subcount tracker / 23/1/2019
 
-from urllib.request import urlopen
+from urllib.request import urlopen, Request 
 from bs4 import BeautifulSoup as soup 
 
-url = "https://twitchstats.net/real-sub-count"
+url = "https://twitchanalysis.top/topsubs_0"
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
+
+
+client = Request(url=url, headers=headers)
+page_html = urlopen(client).read()
+page_soup = soup(page_html, "html.parser")
+
+for row in page_soup.find_all('tr'):
+    for cell in row.find_all('td'):
+        print(cell.text)
