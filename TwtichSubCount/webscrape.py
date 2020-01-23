8c9dfc4ef8e30e2909b19a86b547674305ac3b4a
Arun Arjunan <arun.arjunan1@outlook.com>
2020-01-24 01:11:33 +0800

    added db

--- TwtichSubCount/webscrape.py
+++ TwtichSubCount/webscrape.py
@@ -3,7 +3,7 @@
 from urllib.request import urlopen, Request 
 from bs4 import BeautifulSoup as soup 
 
-url = "https://twitchanalysis.top/topsubs_0"
+url = "https://twitchanalysis.top/topsubs"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
 
 
@@ -11,9 +11,25 @@ client = Request(url=url, headers=headers)
 page_html = urlopen(client).read()
 page_soup = soup(page_html, "html.parser")
 
+db = {}
+table = page_soup.find("table", id="topsubs_table")
 
-database = {}
-for row in page_soup.find_all('tr'):
-    for cell in row.find_all('td'):   
-        print((cell.text).replace('subscriber count', ''))
-    print('\n')
+for cell in page_soup.find_all('td')[3]:
+    cell = page_soup.find_all('td')
+    db[cell[2].text] = [cell[0].text, cell[3].text]
+print(db)
+
+
+# for row in page_soup.find_all('tr')[3]:
+#     for cell in page_soup.find_all('td'):
+#         db[cell[2].text] = [cell[0].text, cell[3].text]
+# print(db)
+
+# with open ('twitch_stats.txt', 'w') as db:
+#     for row in page_soup.find_all('tr'):
+#         for cell in row.find_all('td'): 
+#             print(cell.text('channel'))
+#             db.write(cell.text.ljust(20))
+#         db.write('\n')
+
+
