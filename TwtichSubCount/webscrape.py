9345f65e96eb87d147ca2664cfb12df658cf3dc8
Arun Arjunan <arun.arjunan1@outlook.com>
2020-01-24 12:07:18 +0800

    working build

--- TwtichSubCount/webscrape.py
+++ TwtichSubCount/webscrape.py
@@ -1,35 +1,27 @@
 #twitch subcount tracker / 23/1/2019
-
-from urllib.request import urlopen, Request 
-from bs4 import BeautifulSoup as soup 
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup as soup
 
 url = "https://twitchanalysis.top/topsubs"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
-
-
 client = Request(url=url, headers=headers)
+
 page_html = urlopen(client).read()
 page_soup = soup(page_html, "html.parser")
-
 db = {}
 table = page_soup.find("table", id="topsubs_table")
 
-for cell in page_soup.find_all('td')[3]:
-    cell = page_soup.find_all('td')
-    db[cell[2].text] = [cell[0].text, cell[3].text]
-print(db)
+for row in table.find_all('tr')[1:]:
+    cell = row.find_all('td')
+    if 'This is a spot for an advertisement' in cell[0].text:
+         continue
+    else:
+        db[cell[2].text] = [cell[0].text, cell[3].text]
 
 
-# for row in page_soup.find_all('tr')[3]:
-#     for cell in page_soup.find_all('td'):
-#         db[cell[2].text] = [cell[0].text, cell[3].text]
-# print(db)
+user = input('username ')
 
-# with open ('twitch_stats.txt', 'w') as db:
-#     for row in page_soup.find_all('tr'):
-#         for cell in row.find_all('td'): 
-#             print(cell.text('channel'))
-#             db.write(cell.text.ljust(20))
-#         db.write('\n')
-
-
+if user in db:
+    print(db[user])
+else:
+    print('invalid')
